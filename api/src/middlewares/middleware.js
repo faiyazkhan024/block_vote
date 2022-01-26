7d5f393d658c532af7aca21786afd8e9f0714404
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-01-26 14:54:04 +0530

    Update authorization middleware

--- api/src/middlewares/middleware.js
+++ api/src/middlewares/middleware.js
@@ -3,10 +3,9 @@ const jwt = require("jsonwebtoken");
 const authenticate = (req, res, next) => {
   const authorization = req.headers.authorization;
   const token = authorization.split(" ")[1];
-  if (token === undefined)
-    return res.status(401).json({ message: "Access token undefined" });
+  if (!token) return res.status(401).json({ message: "Access token require" });
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
-    if (err) return res.status(401).json({ message: "Invalid access token" });
+    if (err) return res.status(403).json({ message: "Invalid access token" });
     req.user = result;
     next();
   });
