ff38300dc662355459c5b3cd4c3577644c519f64
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-01-26 16:24:37 +0530

    Update Admin login

--- api/src/controllers/admin.controller.js
+++ api/src/controllers/admin.controller.js
@@ -1,13 +1,13 @@
 const { login } = require("../services/admin.service");
 
-const loginController = async (req, res) => {
-  try {
-    const user = req.body;
-    const result = await login(user);
-    res.status(200).json(result);
-  } catch (error) {
-    res.json(error);
-  }
+const loginController = (req, res) => {
+  const user = req.body;
+  if (!user.username)
+    return res.status(406).json({ message: "Username is required" });
+  if (!user.password)
+    return res.status(406).json({ message: "Password is required" });
+  const result = login(user);
+  res.status(200).json(result);
 };
 
 module.exports = { loginController };
--- api/src/services/admin.service.js
+++ api/src/services/admin.service.js
@@ -1,9 +1,13 @@
-const jwt = require("jsonwebtoken");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../utils/token.util");
 
-const login = async (user) => {
+const login = (user) => {
   const { username } = user;
-  const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET);
-  return { accessToken };
+  const accessToken = generateAccessToken({ username });
+  const refreshToken = generateRefreshToken({ username });
+  return { accessToken, refreshToken };
 };
 
 module.exports = { login };
