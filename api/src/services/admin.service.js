10ccc6491ebfce0476295e5ca97133d40a5bc797
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-01-26 13:30:00 +0530

    Update Admin login Service

--- api/src/services/admin.service.js
+++ api/src/services/admin.service.js
@@ -1,11 +1,9 @@
 const jwt = require("jsonwebtoken");
-const { v4: uuid } = require("uuid");
 
 const login = async (user) => {
-  const id = uuid();
   const { username } = user;
   const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET);
-  return { id, username, accessToken, message: "User Created Successfully" };
+  return { accessToken };
 };
 
 module.exports = { login };
