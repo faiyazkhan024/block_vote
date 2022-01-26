2c13aa2c8d40919e85c18ef171fa4dbbe222bf0f
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-01-26 17:36:15 +0530

    Update app

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
+const createError = require("http-errors");
 require("dotenv").config();
 
 const admin = require("./routes/admin.route");
@@ -10,18 +10,14 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-app.use(helmet());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/admin", admin);
 app.use(token);
+app.use("/admin", admin);
 
-try {
-  app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-  });
-} catch (error) {
-  console.error("Error occurred:", error.message);
-}
+app.use(async (_, __, next) => next(createError.NotFound()));
+app.use((err, _, res, __) => res.status(err.status || 500).json(err));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
