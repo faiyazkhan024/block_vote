941a9c033c309859abe669b1b3a299d798c0d5da
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-02-07 16:16:11 +0530

    Updated Fabric packages

--- api/src/helpers/local.helper.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const getPrivateData = (type) => {
-  const data = fs.readFileSync("../../data/d0.json");
-  return JSON.parse(data)[`${type}s`];
-};
-
-module.exports = { getPrivateData };
--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,33 +1,21 @@
-const bcrypt = require("bcrypt");
-const createError = require("http-errors");
-
 const { generateToken, verifyToken } = require("../helpers/jwt.helper");
-const { getPrivateData } = require("../helpers/local.helper");
 
 const login = async (loginCredential) => {
-  const { username, password, type } = loginCredential;
-  const data = getPrivateData(type);
-  const { id, password: encryptedPass } = data.find(
-    (user) => user.username === username
-  );
-  bcrypt.compare(password, encryptedPass, (err, _) => {
-    if (err) throw createError.BadRequest("Invalid Username or Password");
-  });
-  const accessToken = await generateToken(id);
-  const refreshToken = await generateToken(id, "refresh");
+  const { username } = loginCredential;
+  const accessToken = await generateToken(username);
+  const refreshToken = await generateToken(username, "refresh");
   return { accessToken, refreshToken };
 };
 
 const getToken = async (refreshToken) => {
-  const { id } = await verifyToken(refreshToken, "refresh");
-  const accessToken = await generateToken(id);
+  const { username } = await verifyToken(refreshToken, "refresh");
+  const accessToken = await generateToken(username);
   return { accessToken };
 };
 
 const logout = async (refreshToken) => {
-  const result = await verifyToken(refreshToken, "refresh");
-  const { id } = result;
-  return { id, message: "Logged out" };
+  const { username } = await verifyToken(refreshToken, "refresh");
+  return { username, message: "Logged out" };
 };
 
 module.exports = { login, getToken, logout };
