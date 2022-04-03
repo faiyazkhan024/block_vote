feebb1fa69fab47e415cedd0a91a2bbe39aa096f
Faiyaz Khan <faiyazkhan024@gmail.com>
2022-04-03 16:52:25 +0530

    user login fix

--- api/src/services/auth.js
+++ api/src/services/auth.js
@@ -1,7 +1,18 @@
+const createError = require("http-errors");
+
 const { generateToken, verifyToken } = require("../helpers/jwt");
+const Admin = require("../models/admin");
+const Voter = require("../models/voter");
 
 const login = async (loginCredential, type) => {
-  const { username } = loginCredential;
+  const { username, password } = loginCredential;
+  const user =
+    type === "admin"
+      ? await Admin.findOne({ username })
+      : await Voter.findOne({ username });
+  if (!user) throw new createError.BadRequest("Invalid Username/Password");
+  const isMatch = await user.comparePass(password);
+  if (!isMatch) throw new createError.BadRequest("Invalid Username/Password");
   const accessToken = await generateToken({ username, type });
   const refreshToken = await generateToken({ username, type }, "refresh");
   return { accessToken, refreshToken };
